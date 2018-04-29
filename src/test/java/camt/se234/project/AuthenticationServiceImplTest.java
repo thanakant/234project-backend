e1583e29e31155987e959c023db4f9716d4c2cc3
Thanakan <getgorok4@gmail.com>
2018-04-29 15:41:11 +0700

    Create set up for the new test class
    Add @Before 
    create setup method to set up the mock

--- src/test/java/camt/se234/project/AuthenticationServiceImplTest.java
+++ src/test/java/camt/se234/project/AuthenticationServiceImplTest.java
@@ -1,4 +1,17 @@
 package camt.se234.project;
 
+import camt.se234.project.dao.UserDao;
+import camt.se234.project.service.AuthenticationServiceImpl;
+import org.junit.Before;
+import static org.mockito.Mockito.mock;
+
 public class AuthenticationServiceImplTest {
+ UserDao userDao;
+ AuthenticationServiceImpl authenticationService;
+ @Before
+    public void setUp(){
+     userDao = mock(UserDao.class);
+     authenticationService = new AuthenticationServiceImpl();
+     authenticationService.setUserDao(userDao);
+ }
 }
