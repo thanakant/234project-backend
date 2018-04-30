63206767f4bcfd39fe2be6dcbbec49d2017d3b04
Unknown <chanaphasiri@gmail.com>
2018-05-01 01:30:35 +0700

    Edit OrderDao in set up

--- src/test/java/camt/se234/project/SaleOrderServicelmplTest.java
+++ src/test/java/camt/se234/project/SaleOrderServicelmplTest.java
@@ -1,22 +1,21 @@
 package camt.se234.project;
-import camt.se234.project.dao.OrderDao;
 
+import camt.se234.project.dao.OrderDao;
 import camt.se234.project.dao.OrderDaoImpl;
 import camt.se234.project.service.SaleOrderService;
 import camt.se234.project.service.SaleOrderServiceImpl;
 import org.junit.Before;
-
 import static org.mockito.Mockito.mock;
 
 public class SaleOrderServicelmplTest {
-    OrderDao oderDao;
+    OrderDao orderDao;
     SaleOrderServiceImpl saleOrderService;
 
     @Before
     public void setup(){
-        oderDao = mock(OrderDao.class);
+        orderDao = mock(OrderDao.class);
         saleOrderService = new SaleOrderServiceImpl();
-        saleOrderService.setOrder(orderDao);
+        saleOrderService.setOrderDao(orderDao);
     }
 
 }
