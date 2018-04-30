af3f20421c5f03b386c15852f636685bb8895e6f
Thanakan <sunjido2140@gmail.com>
2018-04-30 22:04:17 +0700

    Create a set up method for ProductServiceImplTest
    add @Before and method setUp

--- src/test/java/camt/se234/project/ProductServiceImplTest.java
+++ src/test/java/camt/se234/project/ProductServiceImplTest.java
@@ -1,4 +1,20 @@
 package camt.se234.project;
 
+import camt.se234.project.dao.ProductDao;
+import camt.se234.project.service.ProductServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+
 public class ProductServiceImplTest {
+    ProductDao productDao;
+    ProductServiceImpl productService;
+    @Before
+    public void setUp(){
+        productDao = mock(ProductDao.class);
+        productService = new ProductServiceImpl();
+        productService.setProductDao(productDao);
+    }
+
+
 }
