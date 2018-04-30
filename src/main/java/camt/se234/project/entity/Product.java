1a07b3b304220080178a9c5f0641965f2130939c
Thanakan <sunjido2140@gmail.com>
2018-04-30 21:48:53 +0700

    Create getter and setter for product class
    Generate getter and setter methods for all attributes in product class

--- src/main/java/camt/se234/project/entity/Product.java
+++ src/main/java/camt/se234/project/entity/Product.java
@@ -25,4 +25,51 @@ public class Product {
     String imageLocation;
     double price;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageLocation() {
+        return imageLocation;
+    }
+
+    public void setImageLocation(String imageLocation) {
+        this.imageLocation = imageLocation;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
