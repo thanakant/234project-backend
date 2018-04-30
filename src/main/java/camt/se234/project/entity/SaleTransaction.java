cd968520ad184d4c9bab7776a3a27741b40cc33f
Unknown <chanaphasiri@gmail.com>
2018-05-01 01:20:30 +0700

    Generate getter and setter in transaction

--- src/main/java/camt/se234/project/entity/SaleTransaction.java
+++ src/main/java/camt/se234/project/entity/SaleTransaction.java
@@ -26,6 +26,54 @@ public class SaleTransaction {
     Product product;
     int amount;
 
+    public SaleTransaction(String transactionId, Long id, SaleOrder order, Product product,int amount){
+        this.transactionId = transactionId;
+        this.id = id;
+        this.order = order;
+        this.product = product;
+        this.amount = amount;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public SaleOrder getOrder() {
+        return order;
+    }
+
+    public void setOrder(SaleOrder order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
 
 
